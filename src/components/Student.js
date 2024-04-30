9e026683b2ccb06a5d4af7992520258ae34ce3f1
Ravindu Malinga <ravindu_d@epiclanka.net>
2024-04-30 13:52:51 +0530

    app bar added

---
+++ src/components/Student.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function Student() {
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Student Name" variant="outlined" />
+      <TextField id="outlined-basic" label="Student Address" variant="outlined" />
+    
+    </Box>
+  );
+}
